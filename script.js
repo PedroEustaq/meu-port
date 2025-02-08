b46b6e41a049f7336e6cd5a6b1f04a07fd03e7db
Pedro Eustáquio <pedroeustaquioassad@gmail.com>
2025-02-08 09:06:55 -0300

    Update

--- script.js
+++ script.js
@@ -2,28 +2,38 @@
 var lastO = document.querySelectorAll(".hidden");
 var Slast = document.querySelectorAll(".Shidden");
 
-var myObserver = new IntersectionObserver( (abacate) => {
-abacate.forEach( (contar) => {
-    if (contar.isIntersecting) {
-        contar.target.classList.add('show');
+var myObserver = new IntersectionObserver((abacate) => {
+    if (window.innerWidth <= 768) {
+        console.log("Script desativado no mobile");
+        return; // Sai do script se for mobile
     } else {
-        contar.target.classList.remove('show');
-    }
-})
+    abacate.forEach((contar) => {
+        if (contar.isIntersecting) {
+            contar.target.classList.add('show');
+        } else {
+            contar.target.classList.remove('show');
+        }
+    })}
 });
 
-var observaSite = new IntersectionObserver( (obsSite) => {
+var observaSite = new IntersectionObserver((obsSite) => {
+    if (window.innerWidth <= 768) {
+        console.log("Script desativado no mobile");
+        return; // Sai do script se for mobile
+    } else {
     console.log(obsSite);
-    obsSite.forEach( (contarR) => {
+    obsSite.forEach((contarR) => {
         if (contarR.isIntersecting) {
             contarR.target.classList.add('apareceSite');
         } else {
             contarR.target.classList.remove('apareceSite');
         }
-    })
-    });
-lastO.forEach( (lastO) => myObserver.observe(lastO));
-Slast.forEach( (Slast) => observaSite.observe(Slast));
+    })}
+});
+
+
+lastO.forEach((lastO) => myObserver.observe(lastO));
+Slast.forEach((Slast) => observaSite.observe(Slast));
 // ANIMACAO
 
 var S1 = document.getElementById("projetosD");
@@ -43,17 +53,17 @@ function setinhaD() {
     }
     if (siteAgora == 2) {
         S2.style.display = "flex";
-    }else {
+    } else {
         S2.style.display = "none";
     }
     if (siteAgora == 3) {
         S3.style.display = "flex";
-    }else {
+    } else {
         S3.style.display = "none";
     }
     if (siteAgora == 4) {
         S4.style.display = "flex";
-    }else {
+    } else {
         S4.style.display = "none";
     }
 }
@@ -69,33 +79,33 @@ function setinhaE() {
     }
     if (siteAgora == 3) {
         S3.style.display = "flex";
-    }else {
+    } else {
         S3.style.display = "none";
     }
     if (siteAgora == 2) {
         S2.style.display = "flex";
-    }else {
+    } else {
         S2.style.display = "none";
     }
     if (siteAgora == 1) {
         S1.style.display = "flex";
-    }else {
+    } else {
         S1.style.display = "none";
     }
 }
-document.getElementById('Home').addEventListener('click', function() {
+document.getElementById('Home').addEventListener('click', function () {
     document.getElementById('contain').scrollIntoView({ behavior: 'smooth' });
 });
 
-document.getElementById('Sobre').addEventListener('click', function() {
+document.getElementById('Sobre').addEventListener('click', function () {
     document.getElementById('textReal').scrollIntoView({ behavior: 'smooth' });
 });
 
-document.getElementById('Portfolio').addEventListener('click', function() {
+document.getElementById('Portfolio').addEventListener('click', function () {
     document.getElementById('projetosD').scrollIntoView({ behavior: 'smooth' });
 });
 
-document.getElementById('Certificado').addEventListener('click', function() {
+document.getElementById('Certificado').addEventListener('click', function () {
     document.getElementById('CertificadoArea').scrollIntoView({ behavior: 'smooth' });
 });
 
@@ -112,18 +122,18 @@ var psou = document.getElementById("pSou")
 var btn = document.getElementById("chat")
 
 const texto = "Pedro é um desenvolvedor júnior com foco em HTML5, CSS3, JavaScript e PHP. Busca se tornar um desenvolvedor sênior, criando soluções práticas que fazem a diferença no dia a dia de pessoas e empresas."; // O texto que será digitado
-    const elemento = document.getElementById("pSou");
-    let indice = 0;
+const elemento = document.getElementById("pSou");
+let indice = 0;
 
 async function resumir() {
 
     btn.style.animationName = 'escreverChat';
-psou.style.animationName = 'sumirR';  // Reseta a animação
+    psou.style.animationName = 'sumirR';  // Reseta a animação
     await esperar(1200); // Aguarda 2 segundos
     psou.innerHTML = ""
     digitar()
 }
-function mouseENT() { 
+function mouseENT() {
     btn.style.animation = 'none';  // Reseta a animação
     void btn.offsetWidth;          // Trigger de reflow
     btn.style.animation = 'CYOanim 0.5s forwards';
@@ -132,19 +142,19 @@ function mouseENT() {
 
 
 
- function digitar() {
+function digitar() {
     if (indice < texto.length) {
-      elemento.textContent += texto.charAt(indice);
-      indice++;
-      setTimeout(digitar, 50); // Velocidade da digitação em milissegundos
+        elemento.textContent += texto.charAt(indice);
+        indice++;
+        setTimeout(digitar, 50); // Velocidade da digitação em milissegundos
     }
-  }
+}
 
-  function esperar(ms) {
+function esperar(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-  }
+}
 
-  function clickfront() {
+function clickfront() {
     window.open("https://www.frontendmentor.io/profile/PedroEustaq")
-  }
-  
+}
+
